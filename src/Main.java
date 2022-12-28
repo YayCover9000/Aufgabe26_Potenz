da391c9cf66dfd7e89f2197b30a0a9aa7f3d940d
YayCover9000 <yassuanf@googlemail.com>
2022-12-28 15:28:42 +0100

    added negativ exclusion

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+            System.out.println("Basis pls");
+        int basis = sc.nextInt();
+            System.out.println("Potenz pls");
+        int potenz = sc.nextInt();
+        int potenziert = Main.berechnungPotenz(basis, potenz);
     }
+    public static int berechnungPotenz(int basis, int potenz){
+        int ergebnis = 0;
+        int speicher;
+        if(basis < 0 || potenz < 0){
+            System.out.println("Eingabe negativ, try again");
+            return 0;
+        }
+/*
+        while()
+*/
+        return ergebnis;
+    }
+
 }
